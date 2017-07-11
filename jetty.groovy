93bd6bed7ecf703f0716e455cdd77edb1c45a60e
kcmvp <kcheng.mvp@gmail.com>
2017-07-11 15:55:06 +0800

    adjust permission

--- jetty.groovy
+++ jetty.groovy
@@ -1,21 +1,21 @@
 #! /usr/bin/env groovy
 
-@GrabResolver(name='restlet', root='http://repo2.maven.org/maven2/')
+//@GrabResolver(name='restlet', root='http://repo2.maven.org/maven2/')
 @Grab(group='org.eclipse.jetty', module='jetty-webapp', version='9.4.6.v20170531')
 @Grab(group='org.eclipse.jetty', module='jetty-server', version='9.4.6.v20170531')
 @Grab(group='org.eclipse.jetty', module='jetty-servlet', version='9.4.6.v20170531')
 @Grab(group='org.eclipse.jetty', module='apache-jsp', version='9.4.6.v20170531')
 @Grab(group='javax.servlet', module='javax.servlet-api', version='3.1.0')
-//@GrabExclude('org.eclipse.jetty.orbit:javax.servlet')
 import org.eclipse.jetty.server.Server
-import org.eclipse.jetty.servlet.*
+import org.eclipse.jetty.servlet.ServletContextHandler
+import org.eclipse.jetty.webapp.WebAppContext
 
 
 def publishedFolder = args ? args[0] : '.'
 
 def server = new Server(8080)
 def context = new ServletContextHandler(server, '/', ServletContextHandler.SESSIONS)
-def webappContext = new org.eclipse.jetty.webapp.WebAppContext(publishedFolder, '/jetty')
+def webappContext = new WebAppContext(publishedFolder, '/sandbox')
 context.setHandler(webappContext)
 server.start()
 println 'Jetty server started. Press Ctrl+C to stop.'
