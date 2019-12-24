c4b7db74d53be1b0a15a5c08ed22608958652e8a
kcmvp <kcheng.mvp@gmail.com>
2019-12-24 15:47:34 +0800

    code refactor

--- xlsJoin.groovy
+++ xlsJoin.groovy
@@ -1,62 +1,64 @@
 #! /usr/bin/env groovy
+import org.apache.poi.hssf.usermodel.HSSFWorkbook
 
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import static org.apache.poi.ss.usermodel.Cell.*;
-import java.text.DateFormat;
-import static java.text.DateFormat.*;
-import static org.apache.poi.hssf.util.HSSFColor.*;
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import java.text.SimpleDateFormat;
-import org.apache.poi.hssf.usermodel.HSSFCellStyle;
-import org.apache.poi.ss.usermodel.CellStyle;
-import java.util.jar.*;
-import groovy.sql.Sql;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-@Grab(group='org.apache.poi', module='poi-ooxml', version='3.10-beta2')
-@GrabExclude('xml-apis:xml-apis')
+import static org.apache.poi.hssf.util.HSSFColor.*
+import static org.apache.poi.ss.usermodel.Cell.*
+@Grapes([
+		@Grab(group = 'com.google.guava', module = 'guava', version = '18.0'),
+		@Grab(group = 'jaxen', module = 'jaxen', version = '1.1.4'),
+		@Grab(group = 'org.apache.poi', module = 'poi-ooxml', version = '3.17'),
+		@GrabExclude('xml-apis:xml-apis')
+])
 
 def home= System.getProperty("user.home");
-def XLS_A_COL = [0,4,5];
-def XLS_B_COL = [1,5,6];
 
-def xlsa = "C:/Users/xianwche/Desktop/log/2014w03/slc01bpx.xls"
-def xlsb = "C:/Users/xianwche/Desktop/log/2014w03/rel9mat3.xls"
-def wba = new HSSFWorkbook(new FileInputStream(xlsa));
-def wbb = new HSSFWorkbook(new FileInputStream(xlsb));
-def sheeta = wba.getSheetAt(0);
-def sheetb = wbb.getSheetAt(0);
-
-def valueSet = new HashSet<String>();
-def rowIterator = sheeta.rowIterator();
-rowIterator.eachWithIndex {row , idx ->
-	def keyStr = new StringBuffer();
-	XLS_A_COL.each{col ->
-		def cell = row.getCell(col);
-		if(cell) keyStr.append(cell.getStringCellValue());
-	}
-	if(keyStr){
-		valueSet.add(keyStr.toString());
-	}
-}
-rowIterator = sheetb.rowIterator();
-rowIterator.eachWithIndex {row , idx ->
-	def keyStr = new StringBuffer();
-	XLS_B_COL.each{col ->
-		def cell = row.getCell(col);
-		if(cell) keyStr.append(cell.getStringCellValue());
-	}
-	if(valueSet.contains(keyStr.toString())){
-		cell = row.getCell(0);
-		if(!cell) cell = row.createCell(0);
-		cell.setCellValue("Y");
-	}
+if (!args || args.length != 2) {
+	println "Please input file names"
+	return -1
 }
 
+def rowMap =  new HashMap<String,String>()
+def cellRange = 1..4
+args.each {name ->
+	def wba = new HSSFWorkbook(new FileInputStream(name));
+	def rowIterator = wba.getSheetAt(0).rowIterator();
+	rowIterator.eachWithIndex {row , idx ->
+		if (row && row.getCell(0)) {
+			def key = row.getCell(0).getStringCellValue()
+			if (!rowMap.containsKey(key)){
+				def list = [] as List
+				list.add(name)
+				list.add(key)
+				cellRange.each {r ->
+					def cell = row.getCell(r)
+					def value = cell.getStringCellValue()
+					list.add(value)
+				}
+				def valueString =list.join("|")
+				println valueString
+				rowMap.put(key, valueString);
+			}
+		}
+	}
+}
+def wb = new HSSFWorkbook()
+def sheet = wb.createSheet()
+rowMap.values().eachWithIndex { String entry, int i ->
+	def row = sheet.createRow(i)
+	def items = entry.split("|")
+	items.eachWithIndex { String colValue, int colIdx ->
+		def cell = row.createCell(colIdx)
+		cell.setCellValue(colValue)
+	}
 
-FileOutputStream fileOut = new FileOutputStream("${home}/workbook.xls");
-wbb.write(fileOut);
+}
+
+def f = new File("${home}/workbook.xls")
+println "Controller info is generated at : ${f.absolutePath}"
+FileOutputStream fileOut = new FileOutputStream(f);
+wb.write(fileOut);
 fileOut.close();
+
 
 
 
