7c38b60ae7b9b4e8bf993888008f18f553c9ebcb
kcmvp <kcheng.mvp@gmail.com>
2020-04-09 12:40:48 +0800

    change the default value

--- infra/kafka/kafkaCfg.groovy
+++ infra/kafka/kafkaCfg.groovy
@@ -71,8 +71,8 @@ bin {
         LOG_DIR = '"/data0/kafka/log"'
         // 64 ram/ 32 cpu
         KAFKA_HEAP_OPTS='"-Xms6g -Xmx6g"'
-        KAFKA_JVM_PERFORMANCE_OPTS='"-server -XX:MetaspaceSize=96m XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:InitiatingHeapOccupancyPercent=35 ' +
-                '-XX:G1HeapRegionSize=16M -XX:MinMetaspaceFreeRatio=50 -XX:MaxMetaspaceFreeRatio=80"'
+        // KAFKA_JVM_PERFORMANCE_OPTS='"-server -XX:MetaspaceSize=96m XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:InitiatingHeapOccupancyPercent=35 ' +
+        //        '-XX:G1HeapRegionSize=16M -XX:MinMetaspaceFreeRatio=50 -XX:MaxMetaspaceFreeRatio=80"'
     }
 
 }
